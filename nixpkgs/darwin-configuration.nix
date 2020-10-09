74d5ccd8d3813554732e43b030d7d6650f511091
Jared Ramirez <jaredramirez@hey.com>
2020-10-08 18:42:08 -0700

    Add nvf function; Other stuff?

--- nixpkgs/darwin-configuration.nix
+++ nixpkgs/darwin-configuration.nix
@@ -66,7 +66,6 @@ in
       '';
       shellAliases = {
         nv = "nvim";
-        nvf = "nvim (rg_with_ignores | fzf)";
         ws-work = "source $HOME/.config/kitty/workspaces/work.fish";
         ws-roc = "source $HOME/.config/kitty/workspaces/roc.fish";
         nix-env = "direnv allow .";
@@ -105,33 +104,6 @@ in
   environment.pathsToLink =
     [ "/share/nix-direnv"
     ];
-
-  services.yabai.enable = true;
-  services.yabai.package = pkgs.yabai;
-
-  services.spacebar.enable = true;
-  services.spacebar.package = pkgs.spacebar;
-  services.spacebar.config = {
-    position           = "top";
-    height             = 26;
-    spacing_left       = 25;
-    spacing_right      = 15;
-    text_font          = ''"Helvetica Neue:Bold:12.0"'';
-    icon_font          = ''"Font Awesome 5 Free:Regular:12.0"'';
-    background_color   = "0xff202020";
-    foreground_color   = "0xffa8a8a8";
-    space_icon_color   = "0xff458588";
-    power_icon_color   = "0xffcd950c";
-    battery_icon_color = "0xffd75f5f";
-    dnd_icon_color     = "0xffa8a8a8";
-    clock_icon_color   = "0xffa8a8a8";
-    space_icon_strip   = "I II III IV V VI VII VIII IX X";
-    power_icon_strip   = " ";
-    space_icon         = "";
-    clock_icon         = "";
-    dnd_icon           = "";
-    clock_format       = ''"%d/%m/%y %R"'';
-  };
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
