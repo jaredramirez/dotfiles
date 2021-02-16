e82856f65f1fcd8a604d87753804671136225599
Jared Ramirez <jaredramirez@hey.com>
2021-02-15 17:39:44 -0800

    Add darwin flakes support?

--- flake.nix
+++ flake.nix
@@ -1,15 +1,22 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    darwin.url = "github:lnl7/nix-darwin/master";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware }: {
+  outputs = { self, nixpkgs, darwin }: {
     nixosConfigurations.home = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./nix/nixos/configuration.nix
         ./nix/nixos/hardware-configuration.nix
       ];
+    };
+
+    darwinConfigurations."Jareds-MBP-2" = darwin.lib.darwinSystem {
+      inputs = { inherit nixpkgs; };
+      modules = [ ./nix/darwin/configuration.nix ];
     };
   };
 }
--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -8,11 +8,6 @@ let
   common = import ./../common.nix { inherit pkgs; };
 in
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      # /etc/nixos/hardware-configuration.nix
-    ];
-
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
