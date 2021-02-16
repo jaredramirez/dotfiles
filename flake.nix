f41ce6d9a9657b5e2a3e947d4ca0ebca6d124999
Jared Ramirez <jaredramirez@hey.com>
2021-02-15 17:30:08 -0800

    Add nixos hardware config

--- flake.nix
+++ flake.nix
@@ -3,10 +3,13 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs, hardwareConfig }: {
-    nixosConfigurations.home = {
+  outputs = { self, nixpkgs, nixos-hardware }: {
+    nixosConfigurations.home = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./nix/nixos/configuration.nix ];
+      modules = [
+        ./nix/nixos/configuration.nix
+        ./nix/nixos/hardware-configuration.nix
+      ];
     };
   };
 }
--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -10,7 +10,7 @@ in
 {
   imports =
     [ # Include the results of the hardware scan.
-      /etc/nixos/hardware-configuration.nix
+      # /etc/nixos/hardware-configuration.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
